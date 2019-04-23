e037f98142791159cce8f87a6567d3faf3551b4b
Serhii Maksymchuk <smakdealcase@gmail.com>
2019-04-23 13:39:53 +0300

    Imported new function GetSearchTermCount using schema comparison.

---
+++ DBUnitTesting/dbo/Functions/GetSearchTermCount.sql
@@ -0,0 +1,10 @@
+﻿CREATE FUNCTION [dbo].[GetSearchTermCount]
+(	
+	@SearchTerm varchar(50)	 
+)
+RETURNS int 
+AS begin
+    declare @Count Int
+	Select @Count =  count(*) FROM [dbo].[Department] WHERE [DepartmentName] LIKE '%' + @SearchTerm + '%'
+	RETURN @Count;
+end
